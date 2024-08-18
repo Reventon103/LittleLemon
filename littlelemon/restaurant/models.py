c2cccdaf7ffb901fba84b5232ba0eb4b00b3d0d9
Kathiravan S <kathiravansingaravalen@gmail.com>
2024-08-18 18:42:47 +0530

    commit after module 3, ready for capstone

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -3,12 +3,18 @@ from django.db import models
 # Create your models here.
 class Menu(models.Model):
     id=models.IntegerField(primary_key=True)
-    Title=models.CharField(max_length=255)
-    Price=models.DecimalField(max_digits=10, decimal_places=2)
-    Inventory=models.IntegerField()
+    title=models.CharField(max_length=255)
+    price=models.DecimalField(max_digits=10, decimal_places=2)
+    inventory=models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} : {str(self.price)}"
 
 class Booking(models.Model):
     id=models.IntegerField(primary_key=True)
-    Name=models.CharField(max_length=255)
-    No_of_guests=models.IntegerField()
+    name=models.CharField(max_length=255)
+    no_of_guests=models.IntegerField()
     BookingDate=models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.name
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
-from .models import MenuItem
-from .serializers import MenuItemSerializer
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer, UserSerializer
+from django.contrib.auth.models import User
+
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request, "index.html", {})
 
-class MenuItemsView(ListCreateAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
-class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
+
+# Create your views here.
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
